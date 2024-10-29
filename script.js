14dd99fb714ace5f485aa1191d6d23615af680c4
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-10-29 19:51:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const inputs=document.querySelectorAll(".code");
+inputs.forEach((input,index)=>{
+	input.addEventListener("input", () =>{
+		if(input.value.length===1 && index<inputs.length-1)
+		{
+			inputs[index+1].focus();
+		}
+		input.addEventListener("keydown",(event)=>{
+			if(event.key==='Backspace')
+			{
+				if(index>0)
+				{
+					inputs[index].value='';
+					inputs[index-1].focus();
+				}
+			}
+		})
+	})
+})
